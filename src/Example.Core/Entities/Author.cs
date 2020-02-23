a49c9c71aced86b4cd3531472e929bd3f1e679f4
Dean Milojevic <milojevicdean@gmail.com>
2020-02-23 14:37:07 +0100

    Adding entity models for the example

---
+++ src/Example.Core/Entities/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Example.Core.Entities
+{
+    public class Author
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+    }
+}
---
+++ src/Example.Core/Entities/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Example.Core.Entities
+{
+    public class Course
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public Author Author { get; set; }
+
+        public Guid AuthorId { get; set; }
+    }
+}
