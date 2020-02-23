d4e5c319040b9f51998e80ff25ab9d58fa626006
Dean Milojevic <milojevicdean@gmail.com>
2020-02-23 16:16:43 +0100

    Working on the repositories

--- src/Example.Core/Repositories/AuthorsRepository.cs
+++ src/Example.Core/Repositories/AuthorsRepository.cs
@@ -1,10 +1,65 @@
 ﻿using System;
+using Example.Core.Entities;
+using Example.Core.Entities.DbContexts;
+
 namespace Example.Core.Repositories
 {
-    public class AuthorsRepository
+    public class AuthorsRepository : IDisposable
     {
-        public AuthorsRepository()
+        private readonly LibraryContext _context;
+
+        public AuthorsRepository(LibraryContext context)
         {
+            _context = context;
         }
+
+        public void Insert(Author author)
+        {
+            if (author is null)
+            {
+                throw new ArgumentNullException(nameof(author));
+            }
+
+            _context.Authors.Add(author);
+        }
+
+        public void Delete(Author author)
+        {
+            if (author is null)
+            {
+                throw new ArgumentNullException(nameof(author));
+            }
+
+            _context.Authors.Remove(author);
+        }
+
+        public bool Save()
+        {
+            return _context.SaveChanges() >= 0;
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        #endregion
     }
 }
--- src/Example.Core/Repositories/CoursesRepository.cs
+++ src/Example.Core/Repositories/CoursesRepository.cs
@@ -1,10 +1,65 @@
 ﻿using System;
+using Example.Core.Entities;
+using Example.Core.Entities.DbContexts;
+
 namespace Example.Core.Repositories
 {
-    public class CoursesRepository
+    public class CoursesRepository : IDisposable
     {
-        public CoursesRepository()
+        private readonly LibraryContext _context;
+
+        public CoursesRepository(LibraryContext context)
         {
+            _context = context;
         }
+
+        public void Insert(Course course)
+        {
+            if (course is null)
+            {
+                throw new ArgumentNullException(nameof(course));
+            }
+
+            _context.Courses.Add(course);
+        }
+
+        public void Delete(Course course)
+        {
+            if (course is null)
+            {
+                throw new ArgumentNullException(nameof(course));
+            }
+
+            _context.Courses.Remove(course);
+        }
+
+        public bool Save()
+        {
+            return _context.SaveChanges() >= 0;
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        #endregion
     }
 }
