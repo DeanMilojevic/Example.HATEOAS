fc329586ceee1194e0c88679cc878e3e0f1b140f
Dean Milojevic <milojevicdean@gmail.com>
2020-02-23 17:21:35 +0100

    Added PagedResponse for the collections

---
+++ src/Example.Api/Models/PagedResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Example.Api.Models
+{
+    public class PagedResponse<T> : List<T>
+    {
+        public PagedResponse(IEnumerable<T> items, int totalCount, int currentPage, int pageSize)
+        {
+            TotalCount = totalCount;
+            Page = currentPage;
+            Size = pageSize;
+
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+
+            AddRange(items);
+        }
+
+        public int TotalCount { get; }
+        public int Page { get; }
+        public int TotalPages { get; }
+        public int Size { get; }
+
+        public bool HasPrevious => Page > 1;
+        public bool HasNext => Page < TotalPages;
+    }
+}
