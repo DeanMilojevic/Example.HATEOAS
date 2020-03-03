b4f7db77f0633db0e1c60184794e8f225770207c
Dean Milojevic <milojevicdean@gmail.com>
2020-03-03 17:43:46 +0100

    Added default value for seach query

--- src/Example.Api/Models/AuthorsRequest.cs
+++ src/Example.Api/Models/AuthorsRequest.cs
@@ -2,7 +2,7 @@
 {
     public class AuthorsRequest
     {
-        public string SearchQuery { get; set; }
+        public string SearchQuery { get; set; } = "";
         public int Page { get; set; } = 1;
         public int HowMany { get; set; } = 10;
         public string Fields { get; set; } = "";
